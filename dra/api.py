fd34438533eb60789d7030b7692e454da650a731
surya1singh <surya1.singh@gmail.com>
2018-07-27 13:15:29 +0530

    data from a database

--- dra/api.py
+++ dra/api.py
@@ -30,4 +30,16 @@ def home():
 def api_all():
     return jsonify(books)
 
+@app.route('/api/v1/resources/books', methods=['GET'])
+def api_id():
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+    results = []
+    for book in books:
+        if book['id'] == id:
+            results.append(book)
+    return jsonify(results)
+
 app.run()
