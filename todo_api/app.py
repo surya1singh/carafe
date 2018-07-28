c1e5b803fe367dc7a131393daacf9a8b67c5b14e
surya1singh <surya1.singh@gmail.com>
2018-07-28 11:24:22 +0530

    return uri with task

--- todo_api/app.py
+++ todo_api/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, abort
-from flask import make_response, request
+from flask import make_response, request, url_for
 
 app = Flask(__name__)
 
@@ -20,14 +20,14 @@ tasks = [
 
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
-    return jsonify({'task': task[0]})
+    return jsonify({'task': make_public_task(task[0])})
 
 
 @app.errorhandler(404)
@@ -72,6 +72,15 @@ def delete_task(task_id):
         abort(404)
     tasks.remove(task[0])
     return jsonify({'result': True})
-    
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
 if __name__ == '__main__':
     app.run(debug=True)
